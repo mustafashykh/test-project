916b0ab42ff94f2b8ad167d77d90df6b487d588a
mustafasheikh1 <mustafaahmed726@gmail.com>
2020-01-15 22:20:59 +0500

    changes

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const cors = require('cors');
+
+app.use(express.json())
+app.use(cors())
+
+
 app.get('/', (req, res) => {
   res.send('<h1>hello, world rvffervre<h1>')
 })
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,79 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  const user = {
+let users = [
+  {
     name: 'Ahsan',
     age: 47,
     occupation: 'Business',
     relationship: 'Married'
+  },
+  {
+    name: 'Ahmad',
+    age: 23,
+    occupation: 'Freelancer',
+    relationship: 'Shadeed Single'
   }
-  res.send(user)
+]
+
+router.get('/', async (req, res) => {
+  return res.send(users)
+})
+
+router.get('/:name', async (req, res) => {
+  const { name } = req.params;
+  
+  users.forEach(user => {
+    if(user.name === name ){
+      return res.send(user).status(200);
+    }
+  })
+  return res.send('No user found').status(404);
+  
+})
+
+router.post('/', async(req, res) => {
+  const { user } = req.body;
+
+  users.push(user) 
+
+  return res.send(user).status(200);
+})
+
+router.put('/', async(req, res) => {
+  const { updatedUser } = req.body;
+
+  try {
+    users.forEach( user => {
+      if( user.name === updatedUser.name){
+        users = users.filter((x) => {
+          return x != updatedUser
+        })
+        
+        users.push(updatedUser);
+        return res.send(updatedUser).status(200);
+      }
+    })
+    return res.send("no user was found").status(404);
+  }catch(e) {
+    return res.send("something went wrong").status(403);
+  }
+   
+})
+
+
+
+router.delete('/' , async(req, res) => {
+  const { user } = req.body;
+  try{
+    users = users.filter((x) => {
+      return x.name != user.name
+    })
+    return res.send(user).status(200);
+  }
+  catch(e){
+    return res.send('something went wrong').status(403);
+  }
 })
 
 
